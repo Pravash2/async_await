63ec59c39af8d02c95f307868e60c226143d8b9c
pravash <pravashbehera2@gmail.com>
2019-01-20 11:27:37 +0530

    make some changes

--- async_await.js
+++ async_await.js
@@ -1,4 +1,3 @@
-
 // getCustomer(1, (customer) => {
 //   console.log('Customer: ', customer);
 //   if (customer.isGold) {
@@ -12,43 +11,42 @@
 // });
 
 async function notifyCustomer() {
-  const customer = await getCustomer(1);
-  console.log('Customer: ', customer);
-  if (customer.isGold) {
-    const movies = await getTopMovies();
-    console.log('Top movies: ', movies);
-    await sendEmail(customer.email, movies);
-    console.log('Email sent...');
-  }
+	const customer = await getCustomer(1);
+	console.log("Customer: ", customer);
+	if (customer.isGold) {
+		const movies = await getTopMovies();
+		console.log("Top movies: ", movies);
+		await sendEmail(customer.email, movies);
+		console.log("Email sent...");
+	}
 }
 notifyCustomer();
 
-
 function getCustomer(id) {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve({ 
-        id: 1, 
-        name: 'Mosh Hamedani', 
-        isGold: true, 
-        email: 'email' 
-      });
-    }, 4000);  
-  });
+	return new Promise((resolve, reject) => {
+		setTimeout(() => {
+			resolve({
+				id: 1,
+				name: "Pravash",
+				isGold: true,
+				email: "email"
+			});
+		}, 4000);
+	});
 }
 
 function getTopMovies() {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve(['movie1', 'movie2']);
-    }, 4000);
-  });
+	return new Promise((resolve, reject) => {
+		setTimeout(() => {
+			resolve(["movie1", "movie2"]);
+		}, 4000);
+	});
 }
 
 function sendEmail(email, movies) {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve();
-    }, 4000);
-  });
+	return new Promise((resolve, reject) => {
+		setTimeout(() => {
+			resolve();
+		}, 4000);
+	});
 }
